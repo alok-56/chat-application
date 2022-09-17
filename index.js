79e0a2a137c928a08f1c00aebbac0da850c93c58
alokkumar <alokkumar11746@gmal.com>
2022-09-18 03:43:00 +0530

    new commit

--- index.js
+++ index.js
@@ -3,10 +3,8 @@ const express=require('express');
 const app=express();
 const server=require('http').createServer(app);
 const io=require('socket.io')(server);
-const path=require('path');
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname + '/public')));
 
 app.get('/',(req,res)=>{
     res.sendFile(__dirname+'/index.html')
